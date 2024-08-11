2602713c28df10ceb69b10835237690d36dc4507
Sjlvascripter <silvascripter@gmail.com>
2024-08-11 14:31:22 -0300

    primeiro commit

---
+++ tipos-de-dados.py
@@ -0,0 +1,12 @@
+print (15 + 15)
+# Exemplo de soma com dado inteiro
+
+print (1.5 + 1.5)
+# Exemplo de soma com ponto flutuante
+
+print (True)
+print (False)
+# Exemplos de dados Booleanos
+
+Print ("Olá mundo")
+# Exemplo de String
